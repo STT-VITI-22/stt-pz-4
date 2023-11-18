c59a2d4b0288feb9c79370703e668669ff6919d7
VovaStolyrhcuk <vova42n@gmail.com>
2023-11-18 13:53:21 +0200

    Stolyrcuk  пз 4

--- app/libs/Calculator.spec.ts
+++ app/libs/Calculator.spec.ts
@@ -1,37 +1,108 @@
-import {Calculator} from './Calculator';
+import { Calculator } from './Calculator';
+ 
 
-describe('Test suite for Calculator.ts', () => {
+describe('Тестовий набір для Calculator.ts', () => {
   let calculator;
-  beforeAll(() => {
-    let inputResult = document.createElement("input");
-    inputResult.type = "text";
-    inputResult.id = 'dashboard'
-    inputResult.className = "app-result";
 
-    calculator = new Calculator()
+  beforeAll(() => {
+    let inputResult = document.createElement('input');
+    inputResult.type = 'text';
+    inputResult.id = 'dashboard';
+    inputResult.className = 'app-result';
+
+    calculator = new Calculator();
     calculator.dashboard = inputResult;
   });
 
-  it('printDigit should to be defined', () => {
+  it('printDigit повинна бути визначена', () => {
     expect(calculator.printDigit).toBeDefined();
   });
 
-  it('printDigit should add new value', () => {
+  it('printDigit повинна додавати нове значення', () => {
     calculator.printDigit('5');
     calculator.printDigit('5');
     expect(calculator.dashboard.value).toBe('55');
   });
 
-  it('printDigit should to be call in calculator.paste', () => {
+  it('printDigit повинна бути викликана в calculator.paste', () => {
     const onSpy = jest.spyOn(calculator, 'printDigit');
-    calculator.paste()
+    calculator.paste();
     expect(onSpy).toHaveBeenCalled();
   });
 
-  it('printAction should to be defined', () => {
+  it('printAction повинна бути визначена', () => {
     expect(calculator.printAction).toBeDefined();
   });
 
-});
+  
 
+  it('clear повинен очищати вміст', () => {
+    calculator.printDigit('5');
+    calculator.clear();
+    expect(calculator.dashboard.value).toBe('');
+  
+  });
+
+  
+
+  it('clear повинен скидувати всі операції', () => {
+    // Встановлюємо початкове значення на дашборді
+    calculator.dashboard.value = '3=';
+  
+    // Викликаємо метод clear
+    calculator.clear();
+  
+    // Очікуємо, що значення буде змінено на порожню строку
+    expect(calculator.dashboard.value).toBe('');
+  });
+  
+  it('clear повинен скидувати всі операції', () => {
+    // Встановлюємо початкове значення на дашборді
+    calculator.dashboard.value = '3=5.3.';
+  
+    // Виклик методу clear
+    calculator.clear();
+  
+    // Очікуємо, що значення буде очищено
+    expect(calculator.dashboard.value).toBe('');
+  });
+
+  it('clear повинен очищати вміст', () => {
+    calculator.printDigit('5');
+    calculator.clear();
+    expect(calculator.dashboard.value).toBe('');
+  });
+
+ 
+  it('printPercentage повинен переводити число в відсотки', () => {
+    // Встановлюємо початкове значення на дашборді
+    calculator.dashboard.value = '50';
+  
+    // Виклик методу printPercentage
+    calculator.printPercentage();
+  
+    // Очікуємо, що значення буде змінено на "0.5"
+    expect(calculator.dashboard.value).toBe('0.5');
+    // Очікуємо, що знак числа буде змінений
+    calculator.dashboard.value = '50';
+
+    // Виклик методу printPercentage
+    calculator.printPercentage();
+  
+    // Очікуємо, що значення буде змінено на "0.5"
+    expect(calculator.dashboard.value).toBe('0.5');
+  });
+  
+  it('clear повинен скидувати всі операції', () => {
+    // Встановлюємо початкове значення на дашборді
+    calculator.dashboard.value = '3=5.3';
+  
+    // Виклик методу clear
+    calculator.clear();
+  
+    // Очікуємо, що значення буде очищено
+    expect(calculator.dashboard.value).toBe('');
+  
+  });
+});
 
--- app/libs/Calculator.ts
+++ app/libs/Calculator.ts
@@ -1,11 +1,10 @@
-import { evaluate } from 'mathjs'
+import { evaluate } from 'mathjs';
 
 export class Calculator {
-
   actions: Array<string> = ['+', '-', '*', '/', '.', '%'];
   dashboard: HTMLInputElement;
 
-  //constructor
+  // constructor
   constructor() {
     this.dashboard = document.getElementById("dashboard") as HTMLInputElement;
     this.setTheme('theme-one');
@@ -13,48 +12,80 @@ export class Calculator {
 
   printAction(val: string): void {
     if (val === '+/-') {
-      let firstDigit = this.dashboard.value[0]
+      let firstDigit = this.dashboard.value[0];
       if (firstDigit === '-') {
-        this.dashboard.value = this.dashboard.value.slice(1, this.dashboard.value.length)
+        this.dashboard.value = this.dashboard.value.slice(1);
       } else {
-        this.dashboard.value = '-' + this.dashboard.value
+        this.dashboard.value = '-' + this.dashboard.value;
       }
-    } else if (this.actions.includes(this.dashboard.value[this.dashboard.value.length - 1])
-      || this.dashboard.value.length === 0) {
+    } else if (
+      this.actions.includes(
+        this.dashboard.value[this.dashboard.value.length - 1]
+      ) ||
+      this.dashboard.value.length === 0
+    ) {
+      // Якщо останній символ - це операція або довжина дашборду 0, нічого не робимо
     } else {
-      this.dashboard.value += val
+      this.dashboard.value += val;
     }
   }
 
   printDigit(val: string) {
-    this.dashboard.value += val
+    this.dashboard.value += val;
+  }
+
+  printDot() {
+    const lastChar = this.dashboard.value[this.dashboard.value.length - 1];
+    if (!lastChar || !this.actions.includes(lastChar)) {
+        this.dashboard.value += '.';
+    }
+  }
+
+  printPercentage() {
+    const currentValue = parseFloat(this.dashboard.value);
+
+  if (!isNaN(currentValue)) {
+    this.dashboard.value = String(currentValue / 100);
+  }
+  }
+
+  toggleSign() {
+    if (this.dashboard.value !== '' && this.dashboard.value !== '0' && !this.actions.includes(this.dashboard.value[this.dashboard.value.length - 1])) {
+      const currentValue = parseFloat(this.dashboard.value);
+      this.dashboard.value = String(-currentValue);
+  }
   }
 
   solve() {
-    let expression = this.dashboard.value
-    this.dashboard.value = evaluate(expression)
+    let expression = this.dashboard.value;
+
+    if (expression.trim() !== '') {
+      this.dashboard.value = String(evaluate(expression));
+    }
   }
 
-  clr() {
-    this.dashboard.value = ''
+  clear() {
+   
+    this.dashboard.value = '';
+    this.actions.length = 0;
   }
 
-  setTheme(themeName) {
+  setTheme(themeName: string) {
     localStorage.setItem('theme', themeName);
-    document.querySelector('body').className = themeName;
+    document.documentElement.className = themeName;
   }
 
   toggleTheme() {
     let theme = localStorage.getItem('theme');
 
     if (theme === 'theme-second') {
-      theme = 'theme-one'
+      theme = 'theme-one';
     } else if (theme === 'theme-one') {
-      theme = 'theme-second'
+      theme = 'theme-second';
     }
     setTimeout(() => {
       this.setTheme(theme);
-    }, 500)
+    }, 500);
   }
 
   save() {
@@ -62,11 +93,6 @@ export class Calculator {
   }
 
   paste() {
-    this.printDigit(localStorage.getItem('result'))
+    this.printDigit(localStorage.getItem('result'));
   }
-
 }
-
-
-
-
