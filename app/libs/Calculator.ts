5f63cb48be2733fcc8fb81ae1647d6582263fdf9
Bondarchuk24 <bondarcukvladislava2001@gmail.com>
2023-12-04 21:11:17 +0200

    pz_4 v1.0

--- app/libs/Calculator.ts
+++ app/libs/Calculator.ts
@@ -1,72 +1,63 @@
-import { evaluate } from 'mathjs'
+import { evaluate } from 'mathjs';
 
 export class Calculator {
-
   actions: Array<string> = ['+', '-', '*', '/', '.', '%'];
   dashboard: HTMLInputElement;
 
-  //constructor
   constructor() {
     this.dashboard = document.getElementById("dashboard") as HTMLInputElement;
-    this.setTheme('theme-one');
+    this.setTheme(localStorage.getItem('theme') || 'theme-one');
   }
 
   printAction(val: string): void {
     if (val === '+/-') {
-      let firstDigit = this.dashboard.value[0]
-      if (firstDigit === '-') {
-        this.dashboard.value = this.dashboard.value.slice(1, this.dashboard.value.length)
-      } else {
-        this.dashboard.value = '-' + this.dashboard.value
-      }
-    } else if (this.actions.includes(this.dashboard.value[this.dashboard.value.length - 1])
-      || this.dashboard.value.length === 0) {
-    } else {
-      this.dashboard.value += val
+      this.dashboard.value = this.dashboard.value[0] === '-' 
+        ? this.dashboard.value.slice(1) 
+        : '-' + this.dashboard.value;
+    } else if (!this.actions.includes(this.dashboard.value[this.dashboard.value.length - 1])
+               && this.dashboard.value.length !== 0) {
+      this.dashboard.value += val;
     }
   }
 
-  printDigit(val: string) {
-    this.dashboard.value += val
+  printDigit(val: string): void {
+    this.dashboard.value += val;
   }
 
-  solve() {
-    let expression = this.dashboard.value
-    this.dashboard.value = evaluate(expression)
+  solve(): void {
+    try {
+      let expression = this.dashboard.value;
+      this.dashboard.value = evaluate(expression).toString();
+    } catch (error) {
+      this.dashboard.value = 'Error';
+    }
   }
 
-  clr() {
-    this.dashboard.value = ''
+  clr(): void {
+    this.dashboard.value = '';
   }
 
-  setTheme(themeName) {
+  setTheme(themeName: string): void {
     localStorage.setItem('theme', themeName);
-    document.querySelector('body').className = themeName;
+    document.body.className = themeName;
   }
 
-  toggleTheme() {
+  toggleTheme(): void {
     let theme = localStorage.getItem('theme');
-
-    if (theme === 'theme-second') {
-      theme = 'theme-one'
-    } else if (theme === 'theme-one') {
-      theme = 'theme-second'
-    }
+    theme = theme === 'theme-second' ? 'theme-one' : 'theme-second';
     setTimeout(() => {
       this.setTheme(theme);
-    }, 500)
+    }, 500);
   }
 
-  save() {
+  save(): void {
     localStorage.setItem('result', this.dashboard.value);
   }
 
-  paste() {
-    this.printDigit(localStorage.getItem('result'))
+  paste(): void {
+    const savedResult = localStorage.getItem('result');
+    if (savedResult) {
+      this.printDigit(savedResult);
+    }
   }
-
 }
-
-
-
-
