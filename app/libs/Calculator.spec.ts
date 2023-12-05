e4df03263e60064f32a45378c58ca5c6b4bb7cae
Akimi-coder <vladaxl9@gmail.com>
2023-12-05 10:08:01 +0200

    PZ4 done Linnyk

--- app/libs/Calculator.spec.ts
+++ app/libs/Calculator.spec.ts
@@ -32,6 +32,69 @@ describe('Test suite for Calculator.ts', () => {
     expect(calculator.printAction).toBeDefined();
   });
 
+  it('toggleTheme should switch between themes', () => {
+    let theme = localStorage.getItem('theme');
+    if (theme === 'theme-second') {
+      calculator.toggleTheme();
+      expect(localStorage.getItem('theme')).toBe('theme-second');
+    } else if (theme === 'theme-one') {
+      calculator.toggleTheme();
+      expect(localStorage.getItem('theme')).toBe('theme-one');
+    }
+    
+  });
+
+  it('clr should clear the dashboard value', () => {
+    calculator.printDigit('8');
+    calculator.clr();
+    expect(calculator.dashboard.value).toBe('');
+  });
+
+  it('solve should evaluate and update dashboard value', () => {
+    calculator.clr();
+    calculator.printDigit('5');
+    calculator.printAction('*');
+    calculator.printDigit('4');
+    calculator.solve();
+    expect(calculator.dashboard.value).toBe('20');
+    
+  });
+
+  it('solve should evaluate and update dashboard value', () => {
+    calculator.clr();
+    calculator.printDigit('5');
+    calculator.printAction('+');
+    calculator.printDigit('4');
+    calculator.solve();
+    expect(calculator.dashboard.value).toBe('9');
+  });
+
+  it('printAction should update dashboard value correctly', () => {
+    calculator.clr();
+    calculator.printDigit('5');
+    calculator.printAction('-');
+    calculator.printDigit('3');
+    expect(calculator.dashboard.value).toBe('5-3');
+  });
+
+  it('printAction should not add action if last character is already an action', () => {
+    calculator.clr();
+    calculator.printDigit('7');
+    calculator.printAction('-');
+    calculator.printAction('*');
+    expect(calculator.dashboard.value).toBe('7-');
+  });
+
+  it('solve should evaluate and update dashboard value', () => {
+    calculator.clr();
+    calculator.printDigit('16');
+    calculator.printAction('/');
+    calculator.printDigit('4');
+    calculator.solve();
+    expect(calculator.dashboard.value).toBe('4');
+    
+  });
+
 });
 
 
